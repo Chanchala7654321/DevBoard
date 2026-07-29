f5eec0cfb17b4d0faa67723bb6058a13e9f02255
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2026-07-29 09:27:52 +0530

    Setup JSON Server for Render deployment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './pages/Login'
+// import TaskCard from './pages/TaskCard'
+// import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div>
+      <Login />
+      {/* <TaskCard />
+      <Contact /> */}
+    </div>
+  )
+}
+
+export default App
